2243984f112f067c9ba4295b6d77fe1a4eefe4a7
dongdigua <dongdigua@outlook.com>
2021-02-20 22:34:12 +0800

    use json

--- Helper.py
+++ Helper.py
@@ -1,4 +1,5 @@
 import sqlite3 as sq
+import json
 
 class Tlist(object):
     shuju=sq.connect("./shuju.db")
@@ -22,27 +23,15 @@ class Tlist(object):
 
 
 class Stat(object):
+    with open('stat.json'.'w+') as f:
+        json.load(f)
     
-    txt=open("stat.txt",'r+') #要注意此处不清除文件
-    content0=txt.readline()
-    streak=int(content0)
-    content1=txt.readline()
-    points=int(content1)
-    txt.close()
    
     def my_streak(self): #有bug，add不了
         mode=input("[streak]>")
         if mode=="show":
-            txt=open("stat.txt",'r+')
-            firstline=txt.readline()
-            print("your streak is: ",firstline)
-            txt.close()
 
         elif mode=="add":
-            new_streak=Stat.streak + 1
-            txt=open("stat.txt",'r+')
-            txt.write(str(new_streak))
-            txt.close()
 
         else:
             print("error")
@@ -51,23 +40,10 @@ class Stat(object):
     def point(self): #有bug，add不了
         mode=input("[point]>")
         if mode=="show":
-            txt=open("stat.txt",'r+')
-            firstline=txt.readline()
-            secondline=txt.readline()
-            print("your point is: ",secondline)
-            txt.close()
 
         elif mode=="add":
-            new_points=Stat.points + 1
-            txt=open("stat.txt",'r+')
-            txt.write(str(new_points))
-            txt.close()
 
         elif mode=="del":
-            new_points=Stat.points - 10
-            txt=open("stat.txt",'r+')
-            txt.write(str(new_points))
-            txt.close()
 
         else:
             print('error')
